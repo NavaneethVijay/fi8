5b5b3485f0e5692f211b8805916997d09ab7c6b2
NavaneethVijay <NavaneethVijay>
2020-02-02 20:09:09 +0530

    removed coaches

--- js/main.js
+++ js/main.js
@@ -70,31 +70,6 @@ AOS.init({
   onePageClick()
 
   var carousel = function() {
-    $('.home-slider').owlCarousel({
-      loop: false,
-      autoplay: false,
-      margin: 0,
-      animateOut: 'fadeOut',
-      animateIn: 'fadeIn',
-      nav: false,
-      autoplayHoverPause: false,
-      items: 1,
-      navText: [
-        "<span class='ion-md-arrow-back'></span>",
-        "<span class='ion-chevron-right'></span>"
-      ],
-      responsive: {
-        0: {
-          items: 1
-        },
-        600: {
-          items: 1
-        },
-        1000: {
-          items: 1
-        }
-      }
-    })
     $('.carousel-testimony').owlCarousel({
       autoplay: true,
       autoHeight: true,
